35b33998085e35b96df061e63b6cce72a8590edb
Toke Høiland-Jørgensen <toke@toke.dk>
2016-04-01 16:37:15 +0200

    Revert "Fix wrong pointer types in babel_reconfigure_ifaces()."
    This reverts commit 392d5e6f887a55557362161df32996e74f471345.

--- proto/babel/babel.c
+++ proto/babel/babel.c
@@ -1456,14 +1456,14 @@ babel_reconfigure_iface(struct babel_proto *p, struct babel_iface *ifa, struct b
 static void
 babel_reconfigure_ifaces(struct babel_proto *p, struct babel_config *cf)
 {
-  struct babel_iface *ifa;
+  struct iface *iface;
 
-  WALK_LIST(ifa, p->interfaces)
+  WALK_LIST(iface, p->interfaces)
   {
-    if (! ifa->up)
+    if (! (iface->flags & IF_UP))
       continue;
 
-    struct iface *iface = ifa->iface;
+    struct babel_iface *ifa = babel_find_iface(p, iface);
     struct babel_iface_config *ic = (void *) iface_patt_find(&cf->iface_list, iface, NULL);
 
     if (ifa && ic)
